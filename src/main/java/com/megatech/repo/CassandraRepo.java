7ed7e4905999e5c57d4323ae10cca7591da96cfa
Karpov <Karpov03@gmail.com>
2017-02-24 01:37:49 +0530

    Master Head Deleted

--- src/main/java/com/megatech/repo/CassandraRepo.java
+++ src/main/java/com/megatech/repo/CassandraRepo.java
@@ -13,20 +13,13 @@ public interface CassandraRepo extends CrudRepository<EnergyTimeData,Long> {
 	@Query("select * from energytimedata where tagid in(?0) ALLOW FILTERING")
     Iterable<EnergyTimeData> findByTagid(List<Integer> tagid);
 
-<<<<<<< HEAD
+
 	//@Query("select * from energytimedata where tagid in (?0) and timestamps in(?1,?2)")
 	
 	@Query("SELECT * FROM energytimedata WHERE tagid IN (?0) AND timestamps > ?1 AND timestamps <=?2")
     Iterable<EnergyTimeData> findByTagidInAndTimestamps(List<Integer> tagid,Long startTime, Long endTime);
 
 
-	
-	
-	
-=======
-	@Query("select * from energytimeseries where value in(?0) ALLOW FILTERING")
-    Iterable<EnergyTimeSeries> findByDate(List<Float> value);
->>>>>>> origin/master
-	
+
 	
 }
